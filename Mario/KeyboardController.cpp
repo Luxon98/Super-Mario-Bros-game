11782e0ab7daa9ea675d20601ca94150765ba6be
Łukasz Polakiewicz <lukaszp9801@gmail.com>
2020-11-09 23:50:27 +0100

    Refactor the KeyboardController's code

--- Mario/KeyboardController.cpp
+++ Mario/KeyboardController.cpp
@@ -20,135 +20,193 @@ void KeyboardController::handleDownArrow(Player &player)
 	}
 }
 
+void KeyboardController::handleLeftAndRightArrowsWhenStanding(Player &player)
+{
+	if (player.playerMovement.stepsLeft > 0) {
+		player.playerMovement.stepsLeft -= 4;
+	}
+	else if (player.playerMovement.stepsRight > 0) {
+		player.playerMovement.stepsRight -= 4;
+	}
+}
+
+void KeyboardController::handleLeftAndUpArrowsWhenStanding(Player &player)
+{
+	if (player.playerMovement.stepsLeft > 0) {
+		player.playerMovement.stepsLeft += 18;
+		player.playerMovement.stepsUp = 2 * player.playerMovement.stepsLeft;
+	}
+	else {
+		player.playerMovement.stepsLeft = 36;
+		player.playerMovement.stepsUp = 72;
+	}
+
+	SoundController::playJumpEffect(player);
+}
+
+void KeyboardController::handleRightAndUpArrowsWhenStanding(Player &player)
+{
+	if (player.playerMovement.stepsRight > 0) {
+		player.playerMovement.stepsRight += 18;
+		player.playerMovement.stepsUp = 2 * player.playerMovement.stepsRight;
+	}
+	else {
+		player.playerMovement.stepsRight = 36;
+		player.playerMovement.stepsUp = 72;
+	}
+
+	SoundController::playJumpEffect(player);
+}
+
+void KeyboardController::handleLeftArrowWhenStanding(Player &player)
+{
+	if (player.playerMovement.stepsLeft > 0 && player.playerMovement.stepsLeft < 60) {
+		player.playerMovement.stepsLeft += 8;
+	}
+	else {
+		player.playerMovement.stepsLeft = 56;
+	}
+}
+
+void KeyboardController::handleRightArrowWhenStanding(Player &player)
+{
+	if (player.playerMovement.stepsRight > 0 && player.playerMovement.stepsRight < 60) {
+		player.playerMovement.stepsRight += 8;
+	}
+	else {
+		player.playerMovement.stepsRight = 56;
+	}
+}
+
+void KeyboardController::handleUpArrowWhenStanding(Player &player)
+{
+	player.playerMovement.stepsUp = 101;
+	SoundController::playJumpEffect(player);
+}
+
+void KeyboardController::handleLeftAndRightArrowsWhenInAir(Player &player)
+{
+	if (keysState[Direction::Left] && player.playerMovement.stepsLeft == 0) {
+		player.playerMovement.stepsLeft = 24;
+	}
+	else if (keysState[Direction::Right] && player.playerMovement.stepsRight == 0) {
+		player.playerMovement.stepsRight = 24;
+	}
+}
+
+void KeyboardController::handleLeftAndUpArrowsWhenInAir(Player &player)
+{
+	if (player.playerMovement.stepsLeft > 0) {
+		player.playerMovement.stepsLeft += 8;
+	}
+	else {
+		player.playerMovement.stepsLeft = 16;
+	}
+
+	if (!doubleJumpFlag) {
+		player.playerMovement.stepsUp += (player.getY() > 200 ? 45 : 20);
+		doubleJumpFlag = true;
+	}
+}
+
+void KeyboardController::handleRightAndUpArrowsWhenInAir(Player &player)
+{
+	if (player.playerMovement.stepsRight > 0) {
+		player.playerMovement.stepsRight += 8;
+	}
+	else {
+		player.playerMovement.stepsRight = 16;
+	}
+
+	if (!doubleJumpFlag) {
+		player.playerMovement.stepsUp += (player.getY() > 200 ? 45 : 20);
+		doubleJumpFlag = true;
+	}
+}
+
+void KeyboardController::handleUpArrowWhenInAir(Player &player)
+{
+	if (!doubleJumpFlag) {
+		player.playerMovement.stepsUp += (player.getY() > 200 ? 65 : 30);
+		doubleJumpFlag = true;
+	}
+}
+
+void KeyboardController::handleLeftArrowWhenInAir(Player &player)
+{
+	if (player.playerMovement.stepsLeft > 0) {
+		player.playerMovement.stepsLeft += 12;
+	}
+	else {
+		player.playerMovement.stepsLeft = 24;
+	}
+}
+
+void KeyboardController::handleRightArrowWhenInAir(Player &player)
+{
+	if (player.playerMovement.stepsRight > 0) {
+		player.playerMovement.stepsRight += 12;
+	}
+	else {
+		player.playerMovement.stepsRight = 24;
+	}
+}
+
+void KeyboardController::handleArrowsWhenStanding(Player &player)
+{
+	if (keysState[Direction::Left] && keysState[Direction::Right]) {
+		handleLeftAndRightArrowsWhenStanding(player);
+	}
+	else if (keysState[Direction::Left] && keysState[Direction::Up]) {
+		handleLeftAndUpArrowsWhenStanding(player);
+	}
+	else if (keysState[Direction::Right] && keysState[Direction::Up]) {
+		handleRightAndUpArrowsWhenStanding(player);
+	}
+	else if (keysState[Direction::Left]) {
+		handleLeftArrowWhenStanding(player);
+	}
+	else if (keysState[Direction::Right]) {
+		handleRightArrowWhenStanding(player);
+	}
+	else if (keysState[Direction::Up]) {
+		handleUpArrowWhenStanding(player);
+	}
+}
+
+void KeyboardController::handleArrowsWhenInAir(Player &player)
+{
+	if (player.playerMovement.stepsUp == 0) {
+		handleLeftAndRightArrowsWhenInAir(player);
+		return;
+	}
+
+	if (keysState[Direction::Left] && keysState[Direction::Up]) {
+		handleLeftAndUpArrowsWhenInAir(player);
+	}
+	else if (keysState[Direction::Right] && keysState[Direction::Up]) {
+		handleRightAndUpArrowsWhenInAir(player);
+	}
+	else if (keysState[Direction::Up]) {
+		handleUpArrowWhenInAir(player);
+	}
+	else if (keysState[Direction::Left]) {
+		handleLeftArrowWhenInAir(player);
+	}
+	else if (keysState[Direction::Right]) {
+		handleRightArrowWhenInAir(player);
+	}
+}
+
 void KeyboardController::handleArrows(Player &player, World &world)
 {
 	if (!player.movementBlock) {
 		if (isCharacterStandingOnSomething(player, world)) {
 			doubleJumpFlag = false;
-
-			if (keysState[Direction::Left] && keysState[Direction::Right]) {
-				if (player.playerMovement.stepsLeft > 0) {
-					player.playerMovement.stepsLeft -= 4;
-				}
-				else if (player.playerMovement.stepsRight > 0) {
-					player.playerMovement.stepsRight -= 4;
-				}
-				return;
-			}
-			else if (keysState[Direction::Left] && keysState[Direction::Up]) {
-				if (player.playerMovement.stepsLeft > 0) {
-					player.playerMovement.stepsLeft += 18;
-					player.playerMovement.stepsUp = 2 * player.playerMovement.stepsLeft;
-				}
-				else {
-					player.playerMovement.stepsLeft = 36;
-					player.playerMovement.stepsUp = 72;
-				}
-
-				SoundController::playJumpEffect(player);
-				return;
-			}
-			else if (keysState[Direction::Right] && keysState[Direction::Up]) {
-				if (player.playerMovement.stepsRight > 0) {
-					player.playerMovement.stepsRight += 18;
-					player.playerMovement.stepsUp = 2 * player.playerMovement.stepsRight;
-				}
-				else {
-					player.playerMovement.stepsRight = 36;
-					player.playerMovement.stepsUp = 72;
-				}
-
-				SoundController::playJumpEffect(player);
-				return;
-			}
-			else if (keysState[Direction::Left]) {
-				if (player.playerMovement.stepsLeft > 0 && player.playerMovement.stepsLeft < 60) {
-					player.playerMovement.stepsLeft += 8;
-				}
-				else {
-					player.playerMovement.stepsLeft = 56;
-				}
-				return;
-			}
-			else if (keysState[Direction::Right]) {
-				if (player.playerMovement.stepsRight > 0 && player.playerMovement.stepsRight < 60) {
-					player.playerMovement.stepsRight += 8;
-				}
-				else {
-					player.playerMovement.stepsRight = 56;
-				}
-				return;
-			}
-			else if (keysState[Direction::Up]) {
-				player.playerMovement.stepsUp = 101;
-				SoundController::playJumpEffect(player);
-				return;
-			}
+			handleArrowsWhenStanding(player);
 		}
 		else {
-			if (player.playerMovement.stepsUp == 0) {
-				if (keysState[Direction::Left] && player.playerMovement.stepsLeft == 0) {
-					player.playerMovement.stepsLeft = 24;
-				}
-				else if (keysState[Direction::Right] && player.playerMovement.stepsRight == 0) {
-					player.playerMovement.stepsRight = 24;
-				}
-				return;
-			}
-
-			if (keysState[Direction::Left] && keysState[Direction::Up]) {
-				if (player.playerMovement.stepsLeft > 0) {
-					player.playerMovement.stepsLeft += 8;
-				}
-				else {
-					player.playerMovement.stepsLeft = 16;
-				}
-
-				if (!doubleJumpFlag) {
-					player.playerMovement.stepsUp += (player.getY() > 200 ? 45 : 20);
-					doubleJumpFlag = true;
-				}
-				return;
-			}
-			else if (keysState[Direction::Right] && keysState[Direction::Up]) {
-				if (player.playerMovement.stepsRight > 0) {
-					player.playerMovement.stepsRight += 8;
-				}
-				else {
-					player.playerMovement.stepsRight = 16;
-				}
-
-				if (!doubleJumpFlag) {						
-					player.playerMovement.stepsUp += (player.getY() > 200 ? 45 : 20);
-					doubleJumpFlag = true;
-				}
-				return;
-			}
-			else if (keysState[Direction::Up]) {
-				if (!doubleJumpFlag) {						
-					player.playerMovement.stepsUp += (player.getY() > 200 ? 65 : 30);
-					doubleJumpFlag = true;
-				}
-				return;
-			}
-			else if (keysState[Direction::Left]) {
-				if (player.playerMovement.stepsLeft > 0) {
-					player.playerMovement.stepsLeft += 12;
-				}
-				else {
-					player.playerMovement.stepsLeft = 24;
-				}
-				return;
-			}
-			else if (keysState[Direction::Right]) {
-				if (player.playerMovement.stepsRight > 0) {
-					player.playerMovement.stepsRight += 12;
-				}
-				else {
-					player.playerMovement.stepsRight = 24;
-				}
-				return;
-			}
+			handleArrowsWhenInAir(player);
 		}
 	}
 }
@@ -203,4 +261,3 @@ void KeyboardController::forceActions(Player &player, World &world)
 	handleDownArrow(player);
 	handleArrows(player, world);
 }
-
--- Mario/KeyboardController.h
+++ Mario/KeyboardController.h
@@ -19,6 +19,20 @@ private:
 	std::map<Direction, bool> keysState;
 	void handleSpacebar(World &world);
 	void handleDownArrow(Player &player);
+	void handleLeftAndRightArrowsWhenStanding(Player &player);
+	void handleLeftAndUpArrowsWhenStanding(Player &player);
+	void handleRightAndUpArrowsWhenStanding(Player &player);
+	void handleLeftArrowWhenStanding(Player &player);
+	void handleRightArrowWhenStanding(Player &player);
+	void handleUpArrowWhenStanding(Player &player);
+	void handleLeftAndRightArrowsWhenInAir(Player &player);
+	void handleLeftAndUpArrowsWhenInAir(Player &player);
+	void handleRightAndUpArrowsWhenInAir(Player &player);
+	void handleUpArrowWhenInAir(Player &player);
+	void handleLeftArrowWhenInAir(Player &player);
+	void handleRightArrowWhenInAir(Player &player);
+	void handleArrowsWhenStanding(Player &player);
+	void handleArrowsWhenInAir(Player &player);
 	void handleArrows(Player &player, World &world);
 
 public:
